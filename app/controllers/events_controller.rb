df7885934b0c9acb8bc2780a448e34bbfe24c32d
Michal Valasek <michal.valasek@gmail.com>
2012-05-24 12:43:43 +0200

    refactored main menu and events index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,15 +8,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @current_stage = params[:stage].nil? ? current_user.stages.first : Stage.find_by_id(params[:stage])
-    
     @stages = current_user.stages
-    @events = @current_stage.events
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @events }
-    end
   end
 
   # GET /event/new
