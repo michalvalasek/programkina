374d4897c87a93c9d92deeed45e12457fc8e6d6f
Michal Valasek <michal.valasek@gmail.com>
2012-08-04 01:22:30 +0200

    clean up mailer configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,22 +64,6 @@ Programkina::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-  config.action_mailer.default_url_options = { :host => 'programkina.sk' }
   
-  require 'tlsmail'
-  Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.perform_deliveries = true
-  ActionMailer::Base.raise_delivery_errors = true
-  ActionMailer::Base.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => "587",
-    :domain => "gmail.com",
-    :enable_starttls_auto => true,
-    :authentication => :login,
-    :user_name => "michal.valasek@gmail.com",
-    :password => "ligkgvfxnndwzjlf"
-  }
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'programkina.sk' }
 end
---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,16 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.default_charset = "utf-8"
+ActionMailer::Base.raise_delivery_errors = true
+
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :port => "587",
+  :domain => "gmail.com",
+  :enable_starttls_auto => true,
+  :authentication => :login,
+  :user_name => "michal.valasek@gmail.com",
+  :password => "ligkgvfxnndwzjlf"
+}
+
+config.action_mailer.raise_delivery_errors = true
