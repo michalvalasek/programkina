b9e2ccf205b42a75f5f7faa21cf2d8cdcdbee043
Michal Valasek <michal.valasek@gmail.com>
2012-09-04 23:04:15 +0200

    improve unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,5 @@
-root = "/home/deployer/apps/programkina/current"
+app_path = "/home/deployer/apps/programkina"
+root = "#{app_path}/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
@@ -8,6 +9,13 @@ listen "/tmp/unicorn.programkina.sock"
 worker_processes 2
 timeout 30
 
-before_exec do |server|
+bundle_path = "#{app_path}/shared/bundle"
+
+before_exec do |_|
+  paths = (ENV["PATH"] || "").split(File::PATH_SEPARATOR)
+  paths.unshift "#{bundle_path}/bin"
+
+  ENV["PATH"] = paths.uniq.join(File::PATH_SEPARATOR)
+  ENV["GEM_HOME"] = ENV['GEM_PATH'] = bundle_path
   ENV["BUNDLE_GEMFILE"] = "#{root}/Gemfile"
 end
