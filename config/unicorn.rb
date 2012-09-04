308ed7f9ebddbf08e9a023d98edc5f04ea9410a6
Michal Valasek <michal.valasek@gmail.com>
2012-09-04 23:47:35 +0200

    rollback changes in unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,4 @@
-app_path = "/home/deployer/apps/programkina"
-root = "#{app_path}/current"
+root = "/home/deployer/apps/programkina/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
@@ -9,13 +8,6 @@ listen "/tmp/unicorn.programkina.sock"
 worker_processes 2
 timeout 30
 
-bundle_path = "#{app_path}/shared/bundle"
-
-before_exec do |_|
-  paths = (ENV["PATH"] || "").split(File::PATH_SEPARATOR)
-  paths.unshift "#{bundle_path}/bin"
-
-  ENV["PATH"] = paths.uniq.join(File::PATH_SEPARATOR)
-  ENV["GEM_HOME"] = ENV['GEM_PATH'] = bundle_path
+before_exec do |server|
   ENV["BUNDLE_GEMFILE"] = "#{root}/Gemfile"
 end
