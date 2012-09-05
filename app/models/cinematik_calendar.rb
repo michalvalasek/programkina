d7a52ddb1092b194e6b2792c4127499574fff9a8
Michal Valasek <michal.valasek@gmail.com>
2012-09-05 22:44:44 +0200

    fix bug in datetime parsing in cinematik calendar

--- app/models/cinematik_calendar.rb
+++ app/models/cinematik_calendar.rb
@@ -3,11 +3,15 @@ require 'open-uri'
 class CinematikCalendar
   
   GOOGLE_ICS = "https://www.google.com/calendar/ical/10m2lcml6frt9di5vkg5avu460%40group.calendar.google.com/public/basic.ics"
+  LOCAL_TIMEZONE = "Bratislava"
   
   attr_accessor :cal
   
   def initialize(ics = GOOGLE_ICS)
     self.cal = Icalendar::parse(open(ics).read).first
+    self.cal.events.each do |e|
+      e.dtstart = e.dtstart.in_time_zone(LOCAL_TIMEZONE)
+    end
   #rescue *HTTP_ERRORS => error 
   end
   
